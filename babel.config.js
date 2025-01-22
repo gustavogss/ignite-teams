c01204af3c47adabd62c8f26f4f86d1f1506a644
gustavogss <gustavogss.dev@gmail.com>
2025-01-22 11:46:14 -0300

    chore: setup project

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@components": "./src/components",
+            "@screens": "./src/screens",
+            "@utils": "./src/utils",
+            "@assets": "./src/assets",
+            "@routes": "./src/routes",
+            "@storage": "./src/storage",
+            "@theme": "./src/theme",
+          },
+        },
+      ],
+    ],
+  };
+};
